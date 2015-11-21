a5a8cec8a01b69ba4589197bc031677e5670b785
Sesta <sesta.ec5@gmail.com>
2015-11-21 17:53:05 +0900

    処理を分割しました

--- js/analyzeLog.js
+++ js/analyzeLog.js
@@ -3,19 +3,24 @@ vislab.analyzeLog = function(){
     type: "GET",
     url: "log/sample_log.txt",
     success: function( txt ){
-      for( id in  vislab.members){
+      for( id in vislab.members){
         member = vislab.members[ id ];
         if( !member.say_count )
           member.say_count = 0;
-
-        member.nicknames.forEach( function( nickname ){
-          member.say_count += txt.split( nickname ).length - 1;
-        } );
-
-        $( "#member-list" ).append( $( "<li></li>", {
-          "class": "member"
-        } ).text( member.name + " : " + member.say_count + "回"  ) );
+        member.say_count += countName( txt, member.nicknames );
       }
+
+      vislab.showSayCount();
     }
   } );
+
+  var countName = function( log_string, nicknames ){
+    say_count = 0;
+
+    nicknames.forEach( function( nickname ){
+      say_count += log_string.split( nickname ).length - 1;
+    } );
+
+    return say_count;
+  }
 };
